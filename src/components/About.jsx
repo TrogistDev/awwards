00d4884aa6f35b423bd3f03c66f514994e7b5161
Gustavo Ferreira <trogist15496@gmail.com>
2026-02-18 16:53:33 +0100

    responsivity updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
     const clipAnimation = gsap.timeline({
       scrollTrigger: {
         trigger: "#clip",
-        start: "center center",
+        start: "center center-=30px",
         end: "+=800 center",
         scrub: 0.5,
         pin: true,
-        pinSpacing: true,
+        
       },
     });
     clipAnimation.to(".mask-clip-path", {
@@ -25,19 +25,22 @@ const About = () => {
   });
 
   return (
-    <div className="min-h-screen w-screen">
+     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <p className="font-general text-sm uppercase md:text-[10px] ">
-          Welcome to zentry
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
         </p>
+
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's<br />l<b>a</b>rgest shared adventure"
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
           containerClass="mt-5 !text-black text-center"
         />
+
         <div className="about-subtext">
-          <p>The Game of Games begins-your live, now an epic MMORPG</p>
-          <p>
-            Zentry unites every player from countless games and plataform&apos;s
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500 ">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
           </p>
         </div>
       </div>
@@ -47,9 +50,8 @@ const About = () => {
           <img
             src="img/about.webp"
             alt="Background"
-            className="absolute left-0 top-0 size-full object-cover "
+            className="absolute left-0 top-0 size-full object-cover"
           />
-            
         </div>
       </div>
     </div>
